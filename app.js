024c4c63fc7ad2ea0764e66112e9ffa49ff07757
Kenny Grage <kennygrage@gmail.com>
2015-09-25 08:22:10 -0700

    finish README, add some web structure

---
+++ app.js
@@ -0,0 +1,20 @@
+var studentList = angular.module('studentList', ['ui.router']);
+
+studentList.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider.state('home', {
+    url: "",
+    templateUrl: "partials/home.html"
+  });
+
+  $stateProvider.state('sign-in', {
+    url: "/sign-in",
+    templateUrl: "partials/sign-in.html",
+    controller: 'StudentsCtrl'
+  });
+
+  $stateProvider.state('courses.students', {
+    url: "attendance",
+    templateUrl: "partials/attendance.html",
+    controller: 'StudentsCtrl'
+  });
+});
---
+++ services/StudentsFactory.js
@@ -0,0 +1,26 @@
+studentList.factory('UtilitiesFactory', function() {
+  var class = {};
+  //hardcode the class in
+  factory.class = [ { name: "Paco Rameriz", signedIn: false },
+                    { name: "Pablo Sanchez", signedIn: false },
+                    { name: "Maria Guadalupe", signedIn: false },
+                    { name: "Rosa Garcia", signedIn: false },
+                    { name: "Miguel Rodriguez", signedIn: false },
+                    { name: "Teresa Martinez", signedIn: false },
+                    { name: "Javier Lopez", signedIn: false },
+                    { name: "Leticia Perez", signedIn: false },
+                    { name: "Carlos Sanchez", signedIn: false },
+                    { name: "Juan Gomez", signedIn: false },
+                    { name: "Gabriela Flores", signedIn: false },
+                    { name: "Roberto Chavez", signedIn: false },
+                    { name: "Alicia Castillo", signedIn: false },
+                    { name: "Raul Nunez", signedIn: false },
+                    { name: "Manuel Fernandez", signedIn: false },
+                    { name: "Veronica Cortez", signedIn: false },
+                    { name: "Domingo Luna", signedIn: false },
+                    { name: "Margarita DeLeon", signedIn: false },
+                    { name: "Pedro Percado", signedIn: false },
+                    { name: "Martha Escobar", signedIn: false }
+  ];
+  return factory;
+});
